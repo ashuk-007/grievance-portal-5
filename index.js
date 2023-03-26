4fbd885012dd15a094c1771d873c9ab2a370c318
kt0601 <tanishkh062003@gamil.com>
2023-03-26 16:49:37 +0530

    signup

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const port =5000;
 const hbs = require("ejs")
 const alert =  require("alert");
 // const popup = require('popups');
-// var con = mysql.createConnection({
-//     host : "localhost",
-//     user : "root",
-//     password : "tanish@0601",
-//     database  : "Grievence"
-    
-    
-// });
+var con = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+   password : "tanish@0601",
+    database  : "gri"
+});
 app.use(express.json());
 app.set("view engine","ejs");
 app.use(express.urlencoded({ extended: false }));    
@@ -26,40 +24,51 @@ app.get("/userLogin",(req,res)=>{
     res.render("userLogin.ejs");
 })
 
+app.get("/officerlogin",(req,res)=>{
+    res.render("userLogin.ejs");
+})
+app.get("/register",(req,res)=>{
+    res.render("register.ejs");
+})
 
-// app.post("/userlogin",(req,res)=>{
-//     console.log("user want to login");
-//   userid =req.body.email;
-//     var password= req.body.password; 
-//                let check=0;
+app.post("/signup",(req,res)=>{
 
-//     con.connect(function(err) {
-//         if (err) throw err;
-//         con.query("SELECT * FROM person where email=(?)",[userid] ,function (err, result, fields) {
-//           if (err) throw err;
-        
-//           console.log(userid);
+    var name = req.body.Name;
+    var password = req.body.password;
+    var aadharNumber = req.body.aadharNumber;
+    var mobileNumber= req.body.mobileNumber;
+    var ageNumber= req.body.ageNumber;
+    var gender = req.body.gender;
+    var email = req.body.email;
+    con.connect(function(err) {
+     if (err) throw err;
 
-//             if(result[0].password==password){
-//                 check=1;
-//                 userid=result[0].user_id;
-//                 console.log(userid);
+   let check =true;
+    //  while(check){
+     let user_id= Math.floor(Math.random() * 100000);
+     let person_id = "user"+user_id;
+     con.query("SELECT count(*) FROM person where person_id=(?)",[person_id] ,function (err, result, fields) {
+                   console.log(result[0]);
+                   console.log("djnckjd");
+         if (err) throw err;
+       else if(result[0]==0){
+            check =false;
+            console.log(check);
 
-//                 res.render("postloginhome.html");
+        }});
+    // }
+    
+         con.query('INSERT INTO person (person_id, person_name, age, gender,aadhar_no,mobile_no,email,pass) VALUES (?, ?, ?, ?, ?,?,?,?)', [person_id,name,ageNumber, gender,aadharNumber ,mobileNumber,email,password],(error, 
+     results) => {
+         if (error)  throw error;
+         else
+        res.render("userlogin");
 
-//             }
-//             else{
-                
-//                 alert("invalid credential");
-//             res.render("login.html",);
-        
-//         }
+         });
+   });
 
-//         });
-//       });
+ });
 
-
-// });
 
 app.listen(port,()=>{
     console.log(`successfully port connected`);
